5cf394c9e8cb34edd8f3fa62b9f8f104bea0a3f4
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-04-15 23:01:17 +0900

    2

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import demo_prac
+
 urlpatterns = [
+    path("", demo_prac.index),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
